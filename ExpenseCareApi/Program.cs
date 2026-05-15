87d69204c2fc32d70e00bd8cf461bcda39e49ec0
Saratha <saratha@sivaneshans-MacBook-Pro-2.local>
2026-05-15 15:20:15 +0530

    fix: add hamburger menu to header

--- ExpenseCareApi/Program.cs
+++ ExpenseCareApi/Program.cs
@@ -92,7 +92,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
-            RoleClaimType = "role"
+            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
                 
         };
     });
